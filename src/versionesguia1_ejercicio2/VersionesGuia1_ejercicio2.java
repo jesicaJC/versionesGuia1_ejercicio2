50ffc9c35fbdb7794011489a9e5621aef1dcb14b
jesicaJC <jesicacondo092@gmail.com>
2023-08-09 16:51:15 -0300

    Initial commit

---
+++ src/versionesguia1_ejercicio2/VersionesGuia1_ejercicio2.java
@@ -0,0 +1,15 @@
+
+package versionesguia1_ejercicio2;
+
+/**
+ *
+ * @author rafa
+ */
+public class VersionesGuia1_ejercicio2 {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hola como estas");
+    }
+    
+}
